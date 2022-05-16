d1b54cc7900d46815bf0e412696ad21d2397fa8b
Lakshya <llakshya63gmail.com>
2022-05-16 19:48:01 +0530

    adding and decreasing qty

--- src/CartIems.js
+++ src/CartIems.js
@@ -10,6 +10,29 @@ class CartItem extends React.Component{
             img:""
         }
     }
+    increaseQty=()=>{
+        // to set the state we have two function,if we do not required prev state than we can we use first functioni
+        // this.setState({
+        //     qty:this.state.qty+1
+        // })
+
+        // fuction second
+        this.setState((prevState)=>{
+            return {
+                qty:prevState.qty+1
+            }
+        })
+    }
+    decreaseQty=()=>{
+        this.setState((prevState)=>{
+            if(prevState.qty>1)
+            {
+                return {
+                    qty:prevState.qty-1
+                }
+            }
+        })
+    }
     render(){
         const {price,title,qty}=this.state;
         return(
@@ -22,9 +45,23 @@ class CartItem extends React.Component{
                     <div>Rs {price}</div>
                     <div>Qty:{qty}</div>
                     <div className="cart-item-actions">
-                        <img src="https://cdn-icons-png.flaticon.com/128/992/992651.png" alt="increase" className="action-icons" />
-                        <img src="https://cdn-icons-png.flaticon.com/128/992/992683.png" alt="decrease" className="action-icons" />
-                        <img src="https://t3.ftcdn.net/jpg/00/65/77/26/240_F_65772602_cPboY4Oamnswse6bN2r9ky4hXDQEVNix.jpg" alt="delete" className="action-icons" />
+                        <img
+                         src="https://cdn-icons-png.flaticon.com/128/992/992651.png" 
+                         alt="increase" 
+                         className="action-icons" 
+                         onClick={this.increaseQty}
+                         />
+                        <img 
+                        src="https://cdn-icons-png.flaticon.com/128/992/992683.png" 
+                        alt="decrease" 
+                        className="action-icons" 
+                        onClick={this.decreaseQty}
+                        />
+                        <img src="https://t3.ftcdn.net/jpg/00/65/77/26/240_F_65772602_cPboY4Oamnswse6bN2r9ky4hXDQEVNix.jpg" 
+                        alt="delete" 
+                        className="action-icons" 
+
+                        />
                     </div>
                 </div>
             </div>  
